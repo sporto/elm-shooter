7a6413df0701451dcae64263997e52b28ba82b57
Sebastian <Sebastian@Sebastian-3.local>
2017-08-04 21:54:51 +1000

    Change to elm Graphics

--- Main.elm
+++ Main.elm
@@ -2,17 +2,12 @@ module Main exposing (..)
 
 import Debug
 import Html exposing (..)
-
-
---import Svg
---import Svg.Attributes as SA
-
 import Keyboard
 import AnimationFrame
 import Html.Attributes exposing (width, height, style)
-import WebGL exposing (Mesh, Shader)
-import Math.Matrix4 as Mat4 exposing (Mat4)
-import Math.Vector3 as Vec3 exposing (vec3, Vec3)
+import Collage exposing (..)
+import Element
+import Color
 
 
 -- MODELS
@@ -64,77 +59,17 @@ subscriptions model =
 
 view : Model -> Html msg
 view model =
-    WebGL.toHtml
-        [ width 400
-        , height 200
-        , style [ ( "display", "block" ) ]
+    collage 400
+        400
+        [ player
         ]
-        [ WebGL.entity
-            vertexShader
-            fragmentShader
-            mesh
-            { perspective = perspective (1) }
-        ]
+        |> Element.toHtml
 
 
-perspective : Float -> Mat4
-perspective t =
-    Mat4.mul
-        (Mat4.makePerspective 45 1 0.01 100)
-        (Mat4.makeLookAt (vec3 (4 * cos t) 0 (4 * sin t)) (vec3 0 0 0) (vec3 0 1 0))
-
-
-
--- Mesh
-
-
-type alias Vertex =
-    { position : Vec3
-    , color : Vec3
-    }
-
-
-mesh : Mesh Vertex
-mesh =
-    WebGL.triangles
-        [ ( Vertex (vec3 1 0 0) (vec3 0 0 0)
-          , Vertex (vec3 0 1 0) (vec3 0 0 0)
-          , Vertex (vec3 0 -1 0) (vec3 0 0 0)
-          )
-        ]
-
-
-
--- Shaders
-
-
-type alias Uniforms =
-    { perspective : Mat4 }
-
-
-vertexShader : Shader Vertex Uniforms { vcolor : Vec3 }
-vertexShader =
-    [glsl|
-        attribute vec3 position;
-        attribute vec3 color;
-        uniform mat4 perspective;
-        varying vec3 vcolor;
-        void main () {
-            gl_Position = perspective * vec4(position, 1.0);
-            vcolor = color;
-        }
-    |]
-
-
-fragmentShader : Shader {} Uniforms { vcolor : Vec3 }
-fragmentShader =
-    [glsl|
-        precision mediump float;
-        varying vec3 vcolor;
-        void main () {
-            gl_FragColor = vec4(vcolor, 1.0);
-        }
-    |]
+player : Form
+player =
+    rect 20 20
+        |> filled (Color.rgb 0 0 0)
 
 
 keyCodeToKey : Keyboard.KeyCode -> Key
